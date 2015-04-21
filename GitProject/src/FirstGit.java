7972acd2cfd5dc69be8c61ab3740819e78381e27
michaelcwright <mwrightmichael@gmail.com>
2015-04-20 21:48:12 -0400

    Third changes.

--- GitProject/src/FirstGit.java
+++ GitProject/src/FirstGit.java
@@ -3,7 +3,7 @@ public class FirstGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Changes to class First Git 1.");
+		System.out.println("Changes to class First Git 3.");
 	}
 
 }
--- GitProject/src/SecondGit.java
+++ GitProject/src/SecondGit.java
@@ -2,7 +2,7 @@
 public class SecondGit {
 
 	public static void main(String[] args) {
-		System.out.println("Changes to Second Git 2.");
+		System.out.println("Changes to Second Git 3.");
 	}
 
 }
